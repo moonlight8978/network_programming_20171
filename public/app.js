599d55296cf7c1bca5446114776173f514051de7
Lê Sĩ Bích <moonlight8978@gmail.com>
2017-11-12 13:20:45 +0700

    add post form

---
+++ public/app.js
@@ -0,0 +1,18 @@
+const URL = 'http://localhost:8888/';
+
+$(document)
+  .on('submit', '#form', function (event) {
+    event.preventDefault();
+
+    // let data = new FormData(this);
+    let data = $(this).serialize();
+
+    axios
+      .post(URL, data, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      })
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  });
