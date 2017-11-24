6e0d9f678d51a96efa4019f26856fe8aa306d8a0
Lê Sĩ Bích <moonlight8978@gmail.com>
2017-11-24 19:03:18 +0700

    add example, fix #query_file parameters names

--- server/server/thang.h
+++ server/server/thang.h
@@ -4,29 +4,44 @@
 // Ham truy van file theo cac dieu kien
 //
 // @param file_name [String] ten file can truy van
-// @param params [Array<PARAM>] mang cac dieu kien truy van
-// @param total_param [int] so luong params
-// @param results [Array<PERSON>] mang ket qua tim duoc
+// @param conditions [Array<PARAM>] mang cac dieu kien truy van
+// @param total_conditions [int] so luong dieu kien
+// @param results [Array<PERSON>] mang ket qua tim duoc sau khi doc file
 // @return [int] so luong ket qua tim duoc
 //
 // @example
-//	 RESULT results[20]
-//   params = [{ name: "Bach" }, { age: 14 }]
-//	 int res = query_file("file.txt", params, results)
+//	 PERSON results[20] (cau truc file txt doc "/README.html")
+//   conditions = [{ first_name: "Bach" }, { year_of_birth: 1997 }]
+//	 int res
+//   res = query_file("file.txt", conditions, 2, results)
 //   => res: 0 (khong tim thay)
-//   => res: 1, results: ["Bach 20 depzaivl@gmail.com"] (tim duoc 1 ket qua)
-int query_file(char* file_name, PARAM* params, int total_params, PERSON* results) {
+//   => res: n, results: [person1, person2 ....] (tim duoc n ket qua)
+int query_file(char* file_name, PARAM* conditions, int total_conditions, PERSON* results) {
 	int total_results = 0;
 	results = new PERSON[20];
 	//code
 	return total_results;
 }
 
-// Ham ghi 1 xau vao file
+// Ham ghi 1 xau vao file, 
+//   ghi vao line moi', khong duoc ghi cung line du lieu cu~
 //
 // @param file_name [String] ten file can ghi
 // @param content [String] du lieu can ghi
 // @return [void]
+// 
+// @example
+//   "/log.txt": 
+//     aaaaaaaaaa
+//     bbbbbbbbbb
+//   write_file("log.txt", "cccccc")
+//   => "/log.txt": (sai)
+//     aaaaaaaaaa
+//     bbbbbbbbbbcccccc
+//   => "/log.txt": (dung)
+//     aaaaaaaaaa
+//     bbbbbbbbbb
+//     cccccc
 void write_file(char* file_name, char* content) {
 	//code
 }
