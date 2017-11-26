e6047d45ba08348e4d520b5726d0ad1efa3d465d
Le Si Bich <moonlight8978@gmail.com>
2017-11-26 20:19:25 +0700

    #write_file done, implementing #query_file

--- server/server/bich.h
+++ server/server/bich.h
@@ -92,3 +92,62 @@ void create_response(char* header, PERSON** results, int total_results, char* re
 
   strcat(response, body);
 }
+
+// Ham truy van file theo cac dieu kien
+//
+// @param file_name [String] ten file can truy van
+// @param conditions [Array<PARAM>] mang cac dieu kien truy van
+// @param total_conditions [int] so luong dieu kien
+// @param results [Array<PERSON>] mang ket qua tim duoc sau khi doc file
+// @return [int] so luong ket qua tim duoc
+//
+// @example
+//	 PERSON results[20] (cau truc file txt doc "/README.html")
+//   conditions = [{ first_name: "Bach" }, { year_of_birth: 1997 }]
+//	 int res
+//   res = query_file("file.txt", conditions, 2, results)
+//   => res: 0 (khong tim thay)
+//   => res: n, results: [person1, person2 ....] (tim duoc n ket qua)
+int query_file(char* file_name, PARAM* conditions, int total_conditions, PERSON* results) {
+	int total_results = 0;
+	results = new PERSON[20];
+	
+	FILE* file;
+	file = fopen(file_name, "r");
+	char line[256];
+	while (fgets(line, sizeof(line), file) != NULL) {
+		// read file, save to results
+	}
+
+	fclose(file);
+	return total_results;
+}
+
+// Ham ghi 1 xau vao file, 
+//   ghi vao line moi', khong duoc ghi cung line du lieu cu~
+//
+// @param file_name [String] ten file can ghi
+// @param content [String] du lieu can ghi
+// @return [void]
+// 
+// @example
+//   "/log.txt": 
+//     aaaaaaaaaa
+//     bbbbbbbbbb
+//     new_line (\n)
+//   write_file("log.txt", "cccccc")
+//   => "/log.txt": (sai)
+//     aaaaaaaaaa
+//     bbbbbbbbbb
+//     cccccc
+//   => "/log.txt": (dung)
+//     aaaaaaaaaa
+//     bbbbbbbbbb
+//     cccccc
+//     new_line (\n)
+void write_file(char* file_name, char* content) {
+	FILE* file;
+	file = fopen(file_name, "a");
+	fprintf(file, "%s\n", content);
+	fclose(file);
+}
--- server/server/thang.h
+++ server/server/thang.h
@@ -1,47 +1,4 @@
 // TODO: Thang
 // Viet ham truy van file, ghi file
 
-// Ham truy van file theo cac dieu kien
-//
-// @param file_name [String] ten file can truy van
-// @param conditions [Array<PARAM>] mang cac dieu kien truy van
-// @param total_conditions [int] so luong dieu kien
-// @param results [Array<PERSON>] mang ket qua tim duoc sau khi doc file
-// @return [int] so luong ket qua tim duoc
-//
-// @example
-//	 PERSON results[20] (cau truc file txt doc "/README.html")
-//   conditions = [{ first_name: "Bach" }, { year_of_birth: 1997 }]
-//	 int res
-//   res = query_file("file.txt", conditions, 2, results)
-//   => res: 0 (khong tim thay)
-//   => res: n, results: [person1, person2 ....] (tim duoc n ket qua)
-int query_file(char* file_name, PARAM* conditions, int total_conditions, PERSON* results) {
-	int total_results = 0;
-	results = new PERSON[20];
-	//code
-	return total_results;
-}
 
-// Ham ghi 1 xau vao file, 
-//   ghi vao line moi', khong duoc ghi cung line du lieu cu~
-//
-// @param file_name [String] ten file can ghi
-// @param content [String] du lieu can ghi
-// @return [void]
-// 
-// @example
-//   "/log.txt": 
-//     aaaaaaaaaa
-//     bbbbbbbbbb
-//   write_file("log.txt", "cccccc")
-//   => "/log.txt": (sai)
-//     aaaaaaaaaa
-//     bbbbbbbbbbcccccc
-//   => "/log.txt": (dung)
-//     aaaaaaaaaa
-//     bbbbbbbbbb
-//     cccccc
-void write_file(char* file_name, char* content) {
-	//code
-}
